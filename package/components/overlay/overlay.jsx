6449ec21845e07e7e721c2df92c956ed26e40331
lianhaibo <957377783@qq.com>
2022-04-26 22:09:44 +0800

    overlay 支持disabled

--- package/components/overlay/overlay.jsx
+++ package/components/overlay/overlay.jsx
@@ -3,18 +3,19 @@ import { Teleport, Transition } from "vue";
 export default {
   name: 'LOverlay',
   props: {
-    show: Boolean
+    show: Boolean,
+    disabled: Boolean
   },
   setup(props, { slots }) {
     
     return () => (
       <Teleport to="body">
         <Transition name="l-overlay">
-          <div v-show={ props.show } class="l-overlay">
+          <div v-show={ props.show } class={`l-overlay ${props.disabled ? 'l-overlay__disabled' : ''}`}>
             { slots.default() }
           </div>
         </Transition>
-    </Teleport>
+      </Teleport>
     )
   },
 }
